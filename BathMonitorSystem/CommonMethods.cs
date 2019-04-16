ff70fe8fb3d32659934231d4b88a5b20ae645f74
519250480 <519250480@qq.com>
2019-04-16 18:50:19 +0800

    第一次更改

---
+++ BathMonitorSystem/CommonMethods.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+namespace BathMonitorSystem
+{
+     static class CommonMethods
+    {
+        public static Dictionary<int, FlowData> CurrentValue = new Dictionary<int, FlowData>();
+        public static Dictionary<int, ModTCP> CurrentModbusList = new Dictionary<int, ModTCP>();
+    }
+}
